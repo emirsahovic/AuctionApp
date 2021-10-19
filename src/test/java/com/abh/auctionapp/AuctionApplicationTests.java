10cf565e95aa7037994a01c28dbea3a7d2c64ab9
emirsahovic <emir.sahovic00@gmail.com>
2021-10-19 18:08:07 +0200

    Starter files for project

---
+++ src/test/java/com/abh/auctionapp/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abh.auctionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
