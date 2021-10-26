3763b7ba6f08709263425629ca4a55f38ec6a006
emirsahovic <emir.sahovic00@gmail.com>
2021-10-26 19:13:18 +0200

    Static pages updated

--- frontend/reactapp/src/Pages/About/About.js
+++ frontend/reactapp/src/Pages/About/About.js
@@ -10,28 +10,28 @@ function About() {
             <h1>About Us</h1>
             <div className="about-content">
                 <div className="about-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consequat pretium turpis, in eleifend mi laoreet sed.
-                    Donec ipsum mauris, venenatis sit amet porttitor id, laoreet eu magna. In convallis diam volutpat libero tincidunt semper.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.Duis consequat pretium turpis, in eleifend mi laoreet sed.
+                    Donec ipsum mauris, venenatis sit amet porttitor id, laoreet eu magna.In convallis diam volutpat libero tincidunt semper.
                     Ut aliquet erat rutrum, venenatis lacus ut, ornare lectus.
-                    Quisque congue ex sit amet diam malesuada, eget laoreet quam molestie. In id elementum turpis. Curabitur quis tincidunt mauris.
+                    Quisque congue ex sit amet diam malesuada, eget laoreet quam molestie.In id elementum turpis.Curabitur quis tincidunt mauris.
                     <br />
                     <br />
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consequat pretium turpis, in eleifend mi laoreet sed.
-                    Donec ipsum mauris, venenatis sit amet porttitor id, laoreet eu magna. In convallis diam volutpat libero tincidunt semper.
-                    Ut aliquet erat rutrum, venenatis lacus ut, ornare lectus. Quisque congue ex sit amet diam malesuada, eget laoreet quam molestie.
-                    In id elementum turpis. Curabitur quis tincidunt mauris.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.Duis consequat pretium turpis, in eleifend mi laoreet sed.
+                    Donec ipsum mauris, venenatis sit amet porttitor id, laoreet eu magna.In convallis diam volutpat libero tincidunt semper.
+                    Ut aliquet erat rutrum, venenatis lacus ut, ornare lectus.Quisque congue ex sit amet diam malesuada, eget laoreet quam molestie.
+                    In id elementum turpis.Curabitur quis tincidunt mauris.
                     <br />
                     <br />
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consequat pretium turpis, in eleifend mi laoreet sed.
-                    Donec ipsum mauris, venenatis sit amet porttitor id, laoreet eu magna. In convallis diam volutpat libero tincidunt semper.
-                    Ut aliquet erat rutrum, venenatis lacus ut, ornare lectus. Quisque congue ex sit amet diam malesuada, eget laoreet quam molestie.
-                    In id elementum turpis. Curabitur quis tincidunt mauris.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.Duis consequat pretium turpis, in eleifend mi laoreet sed.
+                    Donec ipsum mauris, venenatis sit amet porttitor id, laoreet eu magna.In convallis diam volutpat libero tincidunt semper.
+                    Ut aliquet erat rutrum, venenatis lacus ut, ornare lectus.Quisque congue ex sit amet diam malesuada, eget laoreet quam molestie.
+                    In id elementum turpis.Curabitur quis tincidunt mauris.
                     <br />
                     <br />
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consequat pretium turpis, in eleifend mi laoreet sed.
-                    Donec ipsum mauris, venenatis sit amet porttitor id, laoreet eu magna. In convallis diam volutpat libero tincidunt semper.
-                    Ut aliquet erat rutrum, venenatis lacus ut, ornare lectus. Quisque congue ex sit amet diam malesuada, eget laoreet quam molestie.
-                    In id elementum turpis. Curabitur quis tincidunt mauris.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.Duis consequat pretium turpis, in eleifend mi laoreet sed.
+                    Donec ipsum mauris, venenatis sit amet porttitor id, laoreet eu magna.In convallis diam volutpat libero tincidunt semper.
+                    Ut aliquet erat rutrum, venenatis lacus ut, ornare lectus.Quisque congue ex sit amet diam malesuada, eget laoreet quam molestie.
+                    In id elementum turpis.Curabitur quis tincidunt mauris.
 
                 </div>
                 <div className="about-images">
