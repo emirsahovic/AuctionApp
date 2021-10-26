aabdce9363b427582398657096c3a0f935316df3
emirsahovic <emir.sahovic00@gmail.com>
2021-10-26 19:14:50 +0200

    Navbar updated

--- frontend/reactapp/src/Common/Header/NavBar.js
+++ frontend/reactapp/src/Common/Header/NavBar.js
@@ -4,40 +4,40 @@ import ig from "../../Images/instagram.png";
 import tw from "../../Images/twitter.png";
 import gp from "../../Images/google plus.png"
 import logo from "../../Images/auction-app-logo.png"
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 function NavBar() {
-  return(
-  <>
-  <div className="nav-upper">
-    <div className="container-upper">
+  return (
+    <>
+      <div className="nav-upper">
+        <div className="container-upper">
 
-      <div className="social-icons">
-         <a href="https://www.facebook.com" rel="noreferrer" target="_blank"><img src={fb} alt="facebook"></img></a>
-         <a href="https://www.instragram.com" rel="noreferrer" target="_blank"><img src={ig} alt="instagram"></img></a>
-         <a href="https://www.twitter.com" rel="noreferrer" target="_blank"><img src={tw} alt="twitter"></img></a>
-         <a href="https://www.google.com" rel="noreferrer" target="_blank"><img src={gp} alt="google plus"></img></a>
+          <div className="social-icons">
+            <a href="https://www.facebook.com" rel="noreferrer" target="_blank"><img src={fb} alt="facebook"></img></a>
+            <a href="https://www.instragram.com" rel="noreferrer" target="_blank"><img src={ig} alt="instagram"></img></a>
+            <a href="https://www.twitter.com" rel="noreferrer" target="_blank"><img src={tw} alt="twitter"></img></a>
+            <a href="https://www.google.com" rel="noreferrer" target="_blank"><img src={gp} alt="google plus"></img></a>
+          </div>
+
+          <div className="login">
+            <a href="#">Login</a>
+            <p>or</p>
+            <a href="#">Create an Account</a>
+          </div>
+
+        </div>
+
       </div>
 
-      <div className="login">
-          <a href="#">Login</a>
-           <p>or</p>
-          <a href="#">Create an Account</a>
-      </div>
-
-    </div>
-
-  </div>
-
-    <div className="container-main-nav">
-       <Link to="/"><img src={logo} alt="logo"></img></Link>
-       <input type="text" placeholder="Try enter: Shoes"></input>
+      <div className="container-main-nav">
+        <Link to="/"><img src={logo} alt="logo"></img></Link>
+        <input type="text" placeholder="Try enter: Shoes"></input>
         <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Shop</a></li>
-            <li><a href="#">My Account</a></li>
+          <li><NavLink to="/" exact activeClassName="selected">Home</NavLink></li>
+          <li><NavLink to="/shop" activeClassName="selected">Shop</NavLink></li>
+          <li><NavLink to="/account" activeClassName="selected">My Account</NavLink></li>
         </ul>
-    </div>
+      </div>
     </>
   );
 }
