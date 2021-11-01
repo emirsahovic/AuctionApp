4084f39b2b39b7abe8638767a3d22591f5f5fdd6
emirsahovic <emir.sahovic00@gmail.com>
2021-11-01 22:26:04 +0100

    Breadcrumb implemented

--- frontend/reactapp/src/common/Breadcrumb/index.js
+++ frontend/reactapp/src/common/Breadcrumb/index.js
@@ -1,39 +1,48 @@
-import React from "react";
-import { useLocation, Link } from "react-router-dom";
-import { Breadcrumb } from "antd";
-
 import './breadCrumb.css';
+import useBreadcrumbs from 'use-react-router-breadcrumbs';
+import { NavLink } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
-const BreadCrumb = () => {
+const routes = [
+  { path: '/shop/about', breadcrumb: "About Us" },
+  { path: '/shop/terms', breadcrumb: "Terms and Conditions" },
+  { path: '/shop/privacy', breadcrumb: "Privacy and Policy" },
+];
+
+const Breadcrumbs = () => {
+  const breadcrumbs = useBreadcrumbs(routes);
   const location = useLocation();
-  const breadCrumbView = () => {
-    const { pathname } = location;
-    const pathnames = pathname.split("/").filter((item) => item);
-    const capatilize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
-    return (
-      <div className="breadcrumb-cont">
-        {console.log(pathname)}
-        <h5 style={pathname === '/shop/about' ? { display: 'block' } : { display: 'none' }}>About Us</h5>
-        <h5 style={pathname === '/shop/terms' ? { display: 'block' } : { display: 'none' }}>Terms and Conditions</h5>
-        <h5 style={pathname === '/shop/privacy' ? { display: 'block' } : { display: 'none' }}>Privacy and Policy</h5>
-        <Breadcrumb>
-          {pathnames.map((name, index) => {
-            const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
-            const isLast = index === pathnames.length - 1;
-            return isLast ? (
-              <Breadcrumb.Item>{capatilize(name)}</Breadcrumb.Item>
-            ) : (
-              <Breadcrumb.Item>
-                <Link to={`${routeTo}`}>{capatilize(name)}</Link>
-              </Breadcrumb.Item>
-            );
-          })}
-        </Breadcrumb>
-      </div >
-    );
-  };
+  const { pathname } = location;
+  const pathnames = pathname.split("/").filter((item) => item);
+  const pages = { 'about': 'About Us', 'terms': 'Terms and Conditions', 'privacy': 'Privacy and policy' };
 
-  return <>{breadCrumbView()}</>;
+
+  return (
+    <div className="breadcrumbs-container">
+      <div>
+        {pathnames.map((name, i) => <h5 key={i}>{pages[name]}</h5>)}
+      </div>
+
+      {
+        breadcrumbs.map(({
+          match,
+          breadcrumb
+        }) => (
+          <div key={match.url}>
+            <span>
+              {routes.find((route) => route.path === match.url) ?
+                (<><NavLink to={match.url.slice(0, match.url.lastIndexOf("/"))} className="breadcrumb-link">
+                  {match.url.slice(1, match.url.lastIndexOf("/")).charAt(0).toUpperCase() + match.url.slice(1, match.url.lastIndexOf("/")).slice(1)}
+                </NavLink>
+                  <span style={{ fontSize: "20px" }}> &#8594;
+                    <span> {breadcrumb} </span>
+                  </span></>) : null}
+            </span>
+          </div>
+        ))
+      }
+    </div >
+  );
 };
 
-export default BreadCrumb;
+export default Breadcrumbs;
