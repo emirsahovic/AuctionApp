1d60dd8198398dd8ba762c8b85111783d39f853b
emirsahovic <emir.sahovic00@gmail.com>
2021-11-03 23:07:03 +0100

    Initial security configuration

--- src/main/java/com/abh/auctionapp/configuration/SecurityConfiguration.java
+++ src/main/java/com/abh/auctionapp/configuration/SecurityConfiguration.java
@@ -1,2 +1,26 @@
-package com.abh.auctionapp.configuration;public class SecurityConfiguration {
+package com.abh.auctionapp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers().authenticated()
+            .antMatchers("/**").permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
