f8f8462d30775c5098077d6ff7a8210b600b900d
emirsahovic <emir.sahovic00@gmail.com>
2021-11-04 11:01:31 +0100

    Getters added

--- src/main/java/com/abh/auctionapp/request/RegisterRequest.java
+++ src/main/java/com/abh/auctionapp/request/RegisterRequest.java
@@ -1,15 +1,10 @@
 package com.abh.auctionapp.request;
 
-import lombok.Data;
-import lombok.AllArgsConstructor;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
 
-@Data
-@AllArgsConstructor
 public class RegisterRequest {
-
     @NotBlank(message = "First name field can't be empty")
     private String firstName;
 
@@ -23,4 +18,20 @@ public class RegisterRequest {
     @NotBlank(message = "Password field can't be empty")
     @Size(min = 8, message = "Password must contain at least 8 characters")
     private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
