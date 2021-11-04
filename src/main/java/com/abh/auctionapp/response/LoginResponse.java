14e95ac69a69a83682c01402c09c1da0cadf0c5f
emirsahovic <emir.sahovic00@gmail.com>
2021-11-04 13:54:43 +0100

    Initial logic for login added

---
+++ src/main/java/com/abh/auctionapp/response/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.abh.auctionapp.response;
+
+import com.abh.auctionapp.model.Person;
+
+public class LoginResponse {
+    private Person person;
+    private String token;
+
+    public LoginResponse() {}
+
+    public LoginResponse(Person person, String token) {
+        this.person = person;
+        this.token = token;
+    }
+}
