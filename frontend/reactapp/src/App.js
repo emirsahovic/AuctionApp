d1b331ba8d7a36e79350e60741b220c5a1f04a11
emirsahovic <emir.sahovic00@gmail.com>
2021-11-08 22:31:16 +0100

    Logged In state and alert added

--- frontend/reactapp/src/App.js
+++ frontend/reactapp/src/App.js
@@ -1,18 +1,45 @@
 import NavBar from './common/Header/index';
 import Footer from './common/Footer/index'
 import Routes from './routes/index'
-import Breadcrumb from './common/Breadcrumb/index'
+import Breadcrumb from './common/Breadcrumb/index';
+import { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Alert } from 'react-bootstrap';
 
 import './App.css';
 
 function App() {
+  const [loggedInState, setLoggedInState] = useState(null);
+  const [alertVisibility, setAlertVisibility] = useState(false);
+  const [message, setMessage] = useState(null);
+  const [type, setType] = useState(null);
+
+  const showMessage = (type, message) => {
+    setMessage(message);
+    setType(type);
+    setAlertVisibility(true);
+    setTimeout(() => {
+      setAlertVisibility(false)
+    }, 4500);
+  }
+
+  const changeLoggedInState = () => {
+    if (loggedInState === null) {
+      setLoggedInState(false);
+      return;
+    }
+    setLoggedInState(!loggedInState);
+  }
+
   return (
     <div>
       <Router>
-        <NavBar />
+        <NavBar loggedInState={loggedInState} />
         <Breadcrumb />
-        <Routes />
+        <Alert show={alertVisibility} variant={type}>
+          {message}
+        </Alert>
+        <Routes changeLoggedInState={changeLoggedInState} showMessage={showMessage} />
         <Footer />
       </Router >
     </div >
