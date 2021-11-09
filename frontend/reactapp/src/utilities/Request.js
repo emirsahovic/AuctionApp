4e83b543275a98d283a2eb75d4f6a7b9dd4e4802
emirsahovic <emir.sahovic00@gmail.com>
2021-11-09 15:09:26 +0100

    POST request for login added

--- frontend/reactapp/src/utilities/Request.js
+++ frontend/reactapp/src/utilities/Request.js
@@ -3,3 +3,7 @@ import axios from 'axios';
 export async function registerUser(user) {
     return await axios.post('http://localhost:8080/auth/register', user);
 };
+
+export const loginUser = async (user) => {
+    return await axios.post('http://localhost:8080/auth/login', user);
+};
