c4d5aab1e20059434dec08637b6edd5e4dfc6639
emirsahovic <emir.sahovic00@gmail.com>
2021-11-15 15:36:45 +0100

    Product Page UI completed

--- frontend/reactapp/src/pages/ProductPage/index.js
+++ frontend/reactapp/src/pages/ProductPage/index.js
@@ -1,14 +1,23 @@
 import { useEffect, useState } from 'react';
-import { withRouter } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 import { getProduct } from '../../utilities/Request';
-import { Button, Form, Image } from 'react-bootstrap';
+import { Button, Form, Image, Nav } from 'react-bootstrap';
 import { IoIosArrowForward } from "react-icons/io";
+import { getToken } from '../../utilities/Auth';
+import moment from 'moment';
 
 import './productPage.css';
 
-const ProductPage = ({ match }) => {
+const ProductPage = ({ match, loggedInState }) => {
 
     const [product, setProduct] = useState(null);
+    const [loggedIn, setLoggedIn] = useState(getToken() !== null);
+
+    useEffect(() => {
+        if (loggedInState !== null)
+            setLoggedIn(loggedIn);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [loggedInState]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -23,36 +32,46 @@ const ProductPage = ({ match }) => {
         <>
             {product !== null ? (
                 <>
+                    <div className="bg-container">
+                        <div className="breadcrumbs-container breadcrumb-single-product" style={{ paddingTop: '15px' }}>
+                            <p>{product.name}</p>
+                            <div>
+                                <NavLink to="/shop">Shop </NavLink> <span style={{ fontSize: "20px", color: "#9b9b9b" }}> &#8594; </span>
+                                <span>Single Product</span>
+                            </div>
+                        </div>
+                    </div>
+
                     <div className="product-container">
                         <div className="images-container">
                             <Image className="big-img-container" key={product.pictures[0].id} height="450px" width="100%"
                                 src={product.pictures[0].url} />
                             <div className="small-img-container">
                                 {product.pictures.map((picture, i) => (
-                                    <div key={i}>
+                                    <div className="flex-images" key={i}>
                                         <Image
                                             width="100px"
-                                            height="100px"
+                                            height="97px"
                                             src={picture.url}
                                         />
                                         <Image
                                             width="100px"
-                                            height="100px"
+                                            height="97px"
                                             src={picture.url}
                                         />
                                         <Image
                                             width="100px"
-                                            height="100px"
+                                            height="97px"
                                             src={picture.url}
                                         />
                                         <Image
                                             width="100px"
-                                            height="100px"
+                                            height="97px"
                                             src={picture.url}
                                         />
                                         <Image
                                             width="100px"
-                                            height="100px"
+                                            height="97px"
                                             src={picture.url}
                                         />
                                     </div>
@@ -73,26 +92,23 @@ const ProductPage = ({ match }) => {
                                     <br />
                                     Number of bids: <span className="purple-text">1</span>
                                     <br />
-                                    Time left: <span className="purple-text">10 weeks 6 days</span>
+                                    Time left: <span className="purple-text">{moment(product.endDate).diff(moment(), 'weeks')} weeks</span>
                                 </div>
                             </div>
                             <div className="bid-input-button">
                                 <Form.Control className="form-control-gray" type="text" placeholder="Enter $57.00 or higher" />
-                                <Button>
+                                {loggedIn ? <Button>
                                     Place bid
                                     <IoIosArrowForward style={{ fontSize: 22, marginLeft: '10px', marginBottom: '3px' }} />
-                                </Button>
+                                </Button> : null}
                             </div>
                             <div>
-                                <div className="info-container" style={{ marginTop: 15 }}>
+                                <div className="info-container" style={{ marginTop: '15px' }}>
                                     <div className="info-title">
                                         <p className="purple-text detail-title">Details</p>
-                                        <p>Seller information</p>
-                                        <p>Customer reviews</p>
                                     </div>
                                     <div className="lines">
                                         <div className="purple-line" />
-                                        <div className="gray-line" />
                                     </div>
                                     <div style={{ color: '#9b9b9b', fontStyle: 'italic' }}>
                                         {product.description}
@@ -102,30 +118,6 @@ const ProductPage = ({ match }) => {
                         </div>
                     </div>
 
-                    <div className="related-products-container">
-                        <h5 className="related-products-title">Related Products</h5>
-                        <div className="related-products-line"></div>
-                        <div className="related-products-grid">
-                            <div className="related-products-grid-item">
-                                <Image height="450px" width="100%"
-                                    src={product.pictures[0].url} />
-                                <h5 className="related-products-item-title">Shoes Collection</h5>
-                                Starts from <span className="purple-text">${product.startPrice}</span>
-                            </div>
-                            <div className="related-products-grid-item">
-                                <Image height="450px" width="100%"
-                                    src={product.pictures[0].url} />
-                                <h5 className="related-products-item-title">Shoes Collection</h5>
-                                Starts from <span className="purple-text">${product.startPrice}</span>
-                            </div>
-                            <div className="related-products-grid-item">
-                                <Image height="450px" width="100%"
-                                    src={product.pictures[0].url} />
-                                <h5 className="related-products-item-title">Shoes Collection</h5>
-                                Starts from <span className="purple-text">${product.startPrice}</span>
-                            </div>
-                        </div>
-                    </div>
                 </>
 
             ) : null}
